58378db315c49f24db1e688db41cf064dab4cdd2
alessandromauro.m@outlook.com <alex.mauro@live.it>
2021-01-02 19:28:28 +0100

    [neo4j_js] aggiunto file 02_generator per la generazione di date casuali

---
+++ neo4j_js/02_generator_data.js
@@ -0,0 +1,23 @@
+//Importiamo le librerie necessarie alla generazione i dati delle persone fittizie
+const { fake } = require('faker');
+var faker = require('faker');
+var fs = require('fs');
+const { exit } = require('process');
+
+// scegliamo di generare contenuti in lingua inglese 
+faker.locale = "en";
+
+var str= "";
+
+//genero date
+for (var i=0; i<100; i++){
+    var aux = faker.date.past().toString();
+    aux = aux.substring(0, 25); //da 0 a 25 significa che non stampa la parte di stringa successiva GMT+01 eccetera
+    str += aux +'\n';
+}
+
+//scriviamo le queries su file
+fs.writeFile('generator_data.txt', str, function(err){
+    if(err) { return console.log(err); } console.log('The file was saved');
+  });
+  
