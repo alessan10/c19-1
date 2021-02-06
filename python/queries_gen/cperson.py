e7578d901aaeb250b65cf85776879997f8b435a3
Andrea Calabretta <andreacalabretta0@gmail.com>
2021-02-06 20:24:36 +0100

    [py] passo avanti sul query_gen.py

--- python/queries_gen/cperson.py
+++ python/queries_gen/cperson.py
@@ -1,12 +1,13 @@
-class Cperson:
+class Person:
   def __init__(self, name, chatid, covid, weekday, day, month, year):
     self.name = name
     self.chatid = chatid
     self.covid = covid
-    self.weekday = weekday
     self.day = day
+    self.weekday = weekday
     self.month = month
     self.year = year
+    
 
   def toString(self):
     print("Name : " + self.name)
@@ -16,3 +17,18 @@ class Cperson:
     print("Day : " + self.day)
     print("Month : " + self.month)
     print("Year : " + self.year)
+
+
+
+
+d = Person("Deb", "-", "positivo", "Mon", "12","Jen", "2021")
+
+
+d.toString()
+
+
+
+
+
+
+
--- python/queries_gen/main.py
+++ python/queries_gen/main.py
@@ -1,4 +1,28 @@
-import cperson
+import sys
+import random
+import cperson as c
+from faker import Faker
 
-p = Person()
+fake = Faker('en_US')
+
+
+totale = sys.argv[1]
+
+print("totale:", totale)
+
+print(fake.name())
+
+#p = c.Person()
+#print(p.name)
+flag = 1
+
+covid = ('positivo','negativo')
+print(covid[0])
+for n in range(int(totale)): 
+  flag = 1
+  print(fake.date_between(start_date='-1y', end_date='today'))
+  #p = c.Person(fake.name(), "-", covid[random.choice([0, 1])])
+
+
+
 
