fadfe5f1238dc8deaa51e9b3f21329f02562f068
Andrea Calabretta <andreacalabretta0@gmail.com>
2021-04-18 09:42:04 +0200

    [c++] classes.hh classes.cpp completed and working fine

--- c++/inheritance/classes.cpp
+++ c++/inheritance/classes.cpp
@@ -1,18 +1,19 @@
 #include "classes.hh"
-
+/*
 Date::Date(string d, string m, string y, string dow){
 	this->day = d;
 	this->month = m;
 	this->year = y;
 	this->dayOfWeek = dow;
 }
+*/
 
 void Date::toString(){
 	cout << dayOfWeek << " " << day << " "<< month << " "<< year << endl;
 }
-
+/*
 Person::Person(string n, string s):name(n),surname(s){}
-
+*/
 Person::~Person(){
 	cout << "Distruttore Person chiamato" << endl;
 }
@@ -24,12 +25,12 @@ void Person::printSurname(){
 	cout << "Surname : " << surname << endl;
 }
 
-
+/*
 CPerson::CPerson(string n, string s, string cvd, string chId, string d, string m, string y, string dow ): Person(n,s), date(d,m,y,dow) {
 	this->covid = cvd;
 	this->chatId = chId;
 }
-
+*/
 
 void CPerson::toString(){
 	cout << name <<" "<< surname <<" "<< covid <<" "<< chatId <<" "<< date.getDay() <<" "<< date.getMonth() <<" "<< date.getYear() <<" "<< date.getDayOfWeek() << endl;
@@ -38,8 +39,9 @@ void CPerson::toString(){
 }
 int main(){
 	Date d = Date("23", "April", "2021", "Wed");
-	d.toString();
 	
+  d.setDay("45");	
+	d.toString();
 	Person *p = new Person("Andrea", "Calabretta");
 	p->printName();
 	delete p;
--- c++/inheritance/classes.hh
+++ c++/inheritance/classes.hh
@@ -10,33 +10,35 @@ class Date{
 	private:
 		string day, month, year, dayOfWeek;
 	public:
-		Date(string d, string m, string y, string dow);
-		void toString();
-		string getDay(){ return day;}
-		string getMonth(){ return month;}
-		string getYear(){ return year;}
+		Date(string d, string m, string y, string dow): day(d),month(m), year(y), dayOfWeek(dow){};
+		void setDay(string d){ day = d; }
+		void setMonth(string m){ month = m; }
+		void setYear(string y){ year = y; }
+		void setDayOfWeek(string dow){ dayOfWeek = dow; }
+		string getDay(){ return day; }
+		string getMonth(){ return month; }
+		string getYear(){ return year; }
 		string getDayOfWeek(){ return dayOfWeek; }
-		
+		void toString();
 };
 
 class Person{
 	protected:
 		string name, surname;
 	public:
-		Person(string n, string s);
+		Person(string n, string s): name(n), surname(s){};
 		~Person();
 		void printName();
 		void printSurname();
 };
 
-class CPerson: public Person {
+class Patient: public Person {
 	private:
 		string covid;
 		string chatId;
 		Date date;
 	public:
-//		CPerson(string n, string s, string cvd, string chId): Person(n,s), covid(cvd), chatId(chId){};
-		CPerson(string n, string s, string cvd, string chId, string d, string m, string y, string dow);
+		Patient(string n, string s, string cvd, string chId, string d, string m, string y, string dow): Person(n,s),covid(cvd), chatId(chId), date(d,m,y,dow){};
 		void toString();
 };
 
