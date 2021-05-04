083e452969e6d651b746cb7c7a286c6c31cec7aa
Andrea Calabretta <andreacalabretta0@gmail.com>
2021-05-04 18:18:34 +0200

    [c#] button for counter added and working

--- cs/app/app/MainPage.xaml.cs
+++ cs/app/app/MainPage.xaml.cs
@@ -14,5 +14,11 @@ namespace app
         {
             InitializeComponent();
         }
+        int count = 0;
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
     }
 }
