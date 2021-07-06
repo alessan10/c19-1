58be80d42de88cf177469c18cb12aa2851a3d4a3
Andrea Calabretta <andreacalabretta0@gmail.com>
2021-07-06 15:55:10 +0200

    [r] path dataread changed

--- r/geo_script.r
+++ r/geo_script.r
@@ -8,7 +8,7 @@ library(cartography)
 #title("Population in 2008")
 
 #my version
-data = read.csv("C:/Users/alexm/Downloads/out.csv") #importo il dataframe
+data = read.csv("") #importo il dataframe
 data.positive.bycountry = subset(data, Covid == "positivo", select = c(Country, Covid)) #creo un subset con solo i positivi
 country.vector = data.positive.bycountry$Country #metto le country in un vettore
 country.vector.counter = table(country.vector) #conto quante sono
