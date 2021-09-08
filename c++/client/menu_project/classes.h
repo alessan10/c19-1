baea6a0258f7b97f678130c3ed73c39dff0c532f
alessan10 <alessandromauro.m@outlook.com>
2021-09-08 12:35:00 +0200

    [C++] removed qint unused from classes.h

--- c++/client/menu_project/classes.h
+++ c++/client/menu_project/classes.h
@@ -29,7 +29,7 @@ class Date{
 class Person{
     protected:
         QString id, name, surname, age, country ;
-        qint64 aage;
+
     public:
         Person(){};
         Person(QString i, QString n, QString s, QString a, QString c): id(i), name(n), surname(s), age(a), country(c){};
